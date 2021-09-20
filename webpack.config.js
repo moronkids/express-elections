f124c3865cbed6619ecf1310ef870ed24e0994ba
Sulistyo <sulistyoardani@gmail.com>
2021-09-20 12:00:02 +0700

    push

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,3 @@
-// module.resolve = {
-//   alias: {
-//     // Config: path.resolve(__dirname, "..", "src", "config"),
-//     // Components: path.resolve(__dirname, "..", "src", "components"),
-//     // Ducks: path.resolve(__dirname, "..", "src", "ducks"),
-//     // Shared: path.resolve(__dirname, "..", "src", "shared"),
-//     App: path.join(__dirname, '..', 'src'),
-//   },
-// };
+module.exports = {
+  mode: 'development',
+};
