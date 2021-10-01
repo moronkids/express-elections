1fe7a1593533d6f7103ff76f7fb11fa7856497b8
Sulistyo <sulistyoardani@gmail.com>
2021-10-01 19:59:18 +0700

    adding origin

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
+/* eslint-disable quotes */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-shadow */
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const express = require('express');
-const http = require('http');
-const socket = require('socket.io');
-const app = require('./app');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const express = require("express");
+const http = require("http");
+const socket = require("socket.io");
+const app = require("./app");
 
 const httpServer = http.createServer(app);
 
 app.use(express.json());
 // from app.js
-dotenv.config({ path: './config.env' });
-const DB = process.env.DB.replace('<DB_PASSWORD>', process.env.DB_PASSWORD);
+dotenv.config({ path: "./config.env" });
+const DB = process.env.DB.replace("<DB_PASSWORD>", process.env.DB_PASSWORD);
 
 mongoose
   .connect(DB, {
@@ -23,24 +24,24 @@ mongoose
   })
   .then((con) => {
     // console.log(con.connection);
-    console.log('DB connection Successfull');
+    console.log("DB connection Successfull");
   });
 
 const { env } = process;
 const port = env.PORT || 5000;
 const ser = app.listen(port, () => {
-  console.log('running');
+  console.log("running");
 });
 // const io = socket.listen(ser);
 const io = socket(ser, {
   cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
+    origin: ["http://localhost:3000", "https://bri-link-alpha.vercel.app"],
+    methods: ["GET", "POST"],
   },
 });
 
-io.on('connection', async (socket) => {
-  socket.on('imclient', (arg) => {
-    io.emit('imclient', 'peli');
+io.on("connection", async (socket) => {
+  socket.on("imclient", (arg) => {
+    io.emit("imclient", "peli");
   });
 });
